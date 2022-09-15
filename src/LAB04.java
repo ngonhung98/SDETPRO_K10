ec7f114dd00c0c49ca1254c6216c511a97ccd353
ngonhung98 <ngothinhung2609hd@gmail.com>
2022-09-15 15:32:34 -0800

    Lesson03

--- src/LAB04.java
+++ src/LAB04.java
@@ -1,2 +1,9 @@
 public class LAB04 {
+    public static void main(String[] args) {
+        int temp=0;
+        int[]array1= new int[]{1,12,16,28,34};
+        int[]array2=new  int[]{1,13,16,27,99};
+        int array1Lgt=array1.length;
+        int array2Lgt=array2.length;
+    }
 }
